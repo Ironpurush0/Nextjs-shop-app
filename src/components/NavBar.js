2bf7c253d466bfac2bda320118a0eb060dd0b8c6
Ironpurush0 <ridamsaxena90.com>
2021-07-18 23:19:00 +0530

    .

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,19 +35,25 @@ const NavBar = () => {
 						d='M4 6h16M4 12h16M4 18h7'
 					/>
 				</svg>
-				<svg
-					xmlns='http://www.w3.org/2000/svg'
-					className='h-10 w-10 mr-7 hidden md:block'
-					fill='none'
-					viewBox='0 0 24 24'
-					stroke='currentColor'>
-					<path
-						strokeLinecap='round'
-						strokeLinejoin='round'
-						strokeWidth={2}
-						d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
-					/>
-				</svg>
+				<div className='flex items-center justify-center'>
+					<span className='mr-4 hidden md:block font-semibold cursor-pointer'>
+						<p>Sign in</p>
+						<p>Username</p>
+					</span>
+					<svg
+						xmlns='http://www.w3.org/2000/svg'
+						className='h-10 w-10 mr-7 hidden md:block'
+						fill='none'
+						viewBox='0 0 24 24'
+						stroke='currentColor'>
+						<path
+							strokeLinecap='round'
+							strokeLinejoin='round'
+							strokeWidth={2}
+							d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+						/>
+					</svg>
+				</div>
 			</div>
 		</header>
 	);
