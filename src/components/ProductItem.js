8804b5348a591d54c8ebd48259890b5b1a50c79d
Ironpurush0 <ridamsaxena90.com>
2021-07-18 13:46:33 +0530

    currencies added.

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,4 +1,16 @@
+import React, { useState } from "react";
+import { StarIcon } from "@heroicons/react/solid";
+
+const currencyFormatter = require("currency-formatter");
+
+const MAX_RATING = 5;
+const MIN_RATING = 1;
+
 const ProductItem = ({ item }) => {
+	const [ratings] = useState(
+		Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+	);
+
 	return (
 		<div className='hover:cursor-pointer flex flex-col justify-between border w-full p-2'>
 			<div className='h-52'>
@@ -13,9 +25,16 @@ const ProductItem = ({ item }) => {
 				<h1 className='text-2xl font-semibold py-5 text-justify'>
 					{item.title}
 				</h1>
+				<span className='flex my-2'>
+					{Array(ratings)
+						.fill()
+						.map((_, i) => (
+							<StarIcon className='h-5' />
+						))}
+				</span>
 				<p className='text-xl'>{item.description}</p>
 				<p className='text-amazon_blue-priceText text-xl font-bold py-5'>
-					$ {item.price}
+					{currencyFormatter.format(item.price, { locale: "en-GB" })}
 				</p>
 			</div>
 			<div className='flex flex-col p-2'>
