a471b8687c5b15bbe1fab0b800c8ae8e0b2f9c9a
Ironpurush0 <ridamsaxena90.com>
2021-07-28 14:02:50 +0530

    work in progress.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -36,20 +36,25 @@ const NavBar = () => {
 						<p className='font-bold'>{!session ? "Sign in" : "Sign out"}</p>
 						<p>{session && `${session.user.name}`}</p>
 					</span>
-					<svg
-						onClick={() => router.push("/checkout")}
-						xmlns='http://www.w3.org/2000/svg'
-						className='h-10 w-10 mr-7 hidden md:block hover:cursor-pointer'
-						fill='none'
-						viewBox='0 0 24 24'
-						stroke='currentColor'>
-						<path
-							strokeLinecap='round'
-							strokeLinejoin='round'
-							strokeWidth={2}
-							d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
-						/>
-					</svg>
+					<span className=''>
+						<div className='right-7 top-4 flex items-center justify-center w-7 h-7 rounded-full absolute bg-black text-white'>
+							<p className='p-2'>0</p>
+						</div>
+						<svg
+							onClick={() => router.push("/checkout")}
+							xmlns='http://www.w3.org/2000/svg'
+							className='h-12 w-12 mr-7 hidden md:block hover:cursor-pointer'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke='currentColor'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth={2}
+								d='M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z'
+							/>
+						</svg>
+					</span>
 				</div>
 				<svg
 					xmlns='http://www.w3.org/2000/svg'
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -4,10 +4,18 @@ const Checkout = () => {
 	return (
 		<div className='h-screen bg-gray-100'>
 			<main className='lg:flex max-w-screen-2xl mx-auto'>
-				<div>
-					<Image />
+				<div className='flex-grow m-5 shadow-sm'>
+					<Image
+						src='/assets/189.jpg'
+						alt='banner'
+						width='1020'
+						height='250'
+						objectFit='contain'
+					/>
 				</div>
-				<div></div>
+				<div className='flex flex-col p-5 space-y-10 bg-white'>
+					<h1 className='text-3xl p-2 font-semibold'>The cart is empty.</h1>
+				</div>
 			</main>
 		</div>
 	);
